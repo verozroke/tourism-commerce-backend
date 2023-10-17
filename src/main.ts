bfbeedd39c64652815df6e5fee40e8610b1c1e9a
verozroke <73778760+verozroke@users.noreply.github.com>
2023-10-17 18:15:20 +0600

    cors enabling

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
   await app.listen(4000);
 }
 bootstrap();
