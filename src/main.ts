f1b865ff897b7a47fca11b0ae293cc18bc389a63
verozroke <73778760+verozroke@users.noreply.github.com>
2023-10-17 18:17:25 +0600

    origin fgix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      'https://tourism-commerce.vercel.app'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
